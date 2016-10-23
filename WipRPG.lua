c189fdfd131138b3b4d3c5b73fbc73f4ecbf3cc3
Tristan Shawn Den Ouden <trisdo687@gmail.com>
2016-10-23 21:39:29 +0300

    Update WipRPG.lua

--- WipRPG.lua
+++ WipRPG.lua
@@ -1,3 +1,38 @@
 //Add on
 
+//Space key
+sp = " "
+
+//Introduction
 print("Welcome to the WIPRPG!")
+print(sp)
+print("start? (y/n?")
+
+gamestrt = "false"
+strt = io.read()
+
+//i.f the user wants to start the game
+if strt == "y" then
+ gamestrt = "true"
+  print(sp)
+else
+  if strt == "Y" then
+    gamestrt = "true"
+    print(sp)
+else 
+    gamestrt = "false"
+    print(sp)
+end
+  end
+
+if gamestrt == "false" then
+  print("Game exited")
+  exit()
+end
+  
+if gamestrt == "true" then 
+ print("Game starting...")
+  print(sp)
+exit
+  
+//Quest1
